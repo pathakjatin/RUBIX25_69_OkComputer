df176ab933e7a006b1a09a57e49528c70d588e30
Jatin Pathak <jat.pat.rt22@dypatil.edu>
2025-01-23 13:07:54 +0530

    changes

--- backend/src/participants/UserData.model.js
+++ backend/src/participants/UserData.model.js
@@ -1,39 +1,80 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  firebaseUid: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   name: {
     type: String,
-    required: true,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
   },
   email: {
     type: String,
     required: true,
+    unique: true
   },
-  phone: {
+  password: {
     type: String,
+    required: true
+  },
+  github: {
+    type: String,
+    required: false
+  },
+  linkedin: {
+    type: String,
+    required: false
   },
   domain: {
     type: String,
-    required: true,
+    required: false
   },
   role: {
     type: String,
-    enum: ['participant', 'host', 'mentor'],
-    required: true,
+    enum: ['host', 'participant', 'mentor'],
+    required: true
+  },
+  organizationName: {
+    type: String,
+    required: function() {
+      return this.role === 'host';
+    }
+  },
+  qualifications: {
+    type: String,
+    required: function() {
+      return this.role === 'mentor';
+    }
   },
   resume: {
-    type: String, // URL or path to the resume
+    type: String,
+    required: function() {
+      return this.role === 'participant';
+    }
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  profilePic: {
+    type: String,
+    required: false
+  }
+}, {
+  timestamps: true
 });
 
-const User = mongoose.model("User", userSchema);
+// Hash password before saving to the database
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
 module.exports = User;
